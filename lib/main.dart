18b8d70b082ab9fec3c2f4a34c6278b14b24619a
Godslovelee <godslovelee@yahoo.com>
2021-09-11 09:45:36 +0100

    "removing-empty-list"

--- lib/main.dart
+++ lib/main.dart
@@ -68,10 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
 
-
-          ],
         ),
       ),
 
